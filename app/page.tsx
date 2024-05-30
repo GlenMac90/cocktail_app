b7eff6a68a8ee52ab3f44eaf744abd10bc2b743c
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-05-30 14:19:02 +0300

    add more commonly used utility classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Home Page</p>
+      <div className="vertical-between size-80">
+        <p>Home Page</p>
+        <p>Home Page</p>
+      </div>
     </main>
   );
 }
