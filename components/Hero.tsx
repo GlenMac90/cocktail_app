eb0081780fabfb9e8009cd95c838dc95969ade53
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-05-31 22:27:39 +0300

    fix styles in Hero section and amend styles.css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,15 +10,17 @@ const Hero = () => {
   return (
     <header className="flex w-full justify-center overflow-hidden bg-[url('/hero-background.png')] bg-cover">
       <div className="flex w-full max-w-3xl flex-col items-center px-5 sm:px-10 xl:max-w-[90rem] xl:flex-row xl:pl-[6.25rem]">
-        <div className="mb-10 flex w-full max-w-2xl flex-col xl:mb-64 xl:mt-[7.5rem] xl:shrink-0">
-          <p className="base-20 text-light-100">Beverages for connoisseurs</p>
-          <h1 className="hero-heading mt-5 text-light-100">
+        <div className="mb-10 flex w-full max-w-lg flex-col xl:mb-64 xl:mt-[7.5rem] xl:max-w-2xl xl:shrink-0">
+          <p className="base-20 self-center text-light-100 xl:self-start">
+            Beverages for connoisseurs
+          </p>
+          <h1 className="hero-heading mt-5 text-center text-light-100 xl:text-start">
             Raise Your Glass Begin a{" "}
             <span className="text-orange-light">Flavorful Cocktail</span>{" "}
             Journey
           </h1>
           <button
-            className="flex-center mt-10 h-16 w-[17.375rem] gap-2.5 bg-orange-light"
+            className="flex-center mt-10 h-12 gap-2.5 bg-orange-light sm:h-16 xl:w-[17.375rem]"
             onClick={handleArrowClick}
           >
             <span className="medium-20">Check Collections</span>
