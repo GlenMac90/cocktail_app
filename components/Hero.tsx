2cf85f84062a1126362d8252e6f50dce3d7d99fb
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-05-31 22:02:54 +0300

    fix styling to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,8 +8,8 @@ const Hero = () => {
   };
 
   return (
-    <header className="flex w-full justify-center bg-[url('/hero-background.png')] bg-cover">
-      <div className="flex w-full max-w-3xl flex-col items-center px-10 xl:max-w-[90rem] xl:flex-row xl:pl-[6.25rem]">
+    <header className="flex w-full justify-center overflow-hidden bg-[url('/hero-background.png')] bg-cover">
+      <div className="flex w-full max-w-3xl flex-col items-center px-5 sm:px-10 xl:max-w-[90rem] xl:flex-row xl:pl-[6.25rem]">
         <div className="mb-10 flex w-full max-w-2xl flex-col xl:mb-64 xl:mt-[7.5rem] xl:shrink-0">
           <p className="base-20 text-light-100">Beverages for connoisseurs</p>
           <h1 className="hero-heading mt-5 text-light-100">
@@ -30,13 +30,13 @@ const Hero = () => {
             />
           </button>
         </div>
-        <figure className="relative h-[44rem] w-full max-w-lg shrink-0 xl:max-w-2xl xl:pl-10">
+        <figure className="flex h-fit w-full max-w-lg shrink-0 items-end xl:h-[44rem] xl:max-w-xl xl:pb-24 xl:pl-14">
           <Image
             src="/drinks-graphic.png"
             alt="A refreshing orange cocktail in a glass"
             height={618}
             width={600}
-            className="absolute object-contain xl:-bottom-8"
+            className="object-contain"
           />
         </figure>
       </div>
