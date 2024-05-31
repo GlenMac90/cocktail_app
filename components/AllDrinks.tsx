f1ee2636de115fec9edafa86abf5e3012dd84d4b
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-06-01 00:34:35 +0300

    reusable card created

--- components/AllDrinks.tsx
+++ components/AllDrinks.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useEffect } from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { useInView } from "react-intersection-observer";
 
 import { getAllDrinks } from "@/lib/actions/drinks.actions";
@@ -10,6 +8,7 @@ import LoadingGraphic from "./LoadingGraphic";
 import { DrinkData, DrinkDataResponse } from "@/types/drinks.index";
 import useFetch from "@/hooks/useFetch";
 import NoMoreDrinks from "./NoMoreDrinks";
+import DrinkCard from "./DrinkCard";
 
 const AllDrinks = ({ data }: { data: DrinkDataResponse }) => {
   const { drinks, isMorePosts, fetching, fetchMoreDrinks } = useFetch({
@@ -31,27 +30,10 @@ const AllDrinks = ({ data }: { data: DrinkDataResponse }) => {
         <h3 className="serif-heading">All Drinks</h3>
         <div className="flex flex-col gap-8 sm:grid sm:grid-cols-2 xl:grid-cols-3 xl:gap-y-[3.125rem]">
           {drinks.map((drink: DrinkData) => (
-            <Link
-              href={`/drink/${drink.id}`}
-              key={drink.id}
-              className="rounded bg-gradient-to-b from-slate-light to-slate-mid p-5"
-            >
-              <figure className="flex flex-col gap-6">
-                <Image
-                  src={drink.image}
-                  height={305}
-                  width={354}
-                  alt="Drink Image"
-                  className="w-full rounded object-contain"
-                />
-                <figcaption>
-                  <h4 className="semibold-24 text-light-100">{drink.name}</h4>
-                </figcaption>
-              </figure>
-            </Link>
+            <DrinkCard key={drink.id} drink={drink} />
           ))}
         </div>
-        <div ref={ref} className="flex-center min-h-10 w-full">
+        <div ref={ref} className="flex-center min-h-60 w-full">
           {fetching && <LoadingGraphic />}
           {!isMorePosts && <NoMoreDrinks />}
         </div>
