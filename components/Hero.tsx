5441199cdf3f3b10055c7c6bf36fc9d0fc1d3bea
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-05-31 22:41:25 +0300

    adjust margin and padding in container of hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,11 +10,11 @@ const Hero = () => {
   return (
     <header className="flex w-full justify-center overflow-hidden bg-[url('/hero-background.png')] bg-cover">
       <div className="flex w-full max-w-3xl flex-col items-center px-5 sm:px-10 xl:max-w-[90rem] xl:flex-row xl:pl-[6.25rem]">
-        <div className="mb-10 flex w-full max-w-lg flex-col xl:mb-64 xl:mt-[7.5rem] xl:max-w-2xl xl:shrink-0">
+        <div className="mb-10 flex w-full max-w-lg flex-col xl:max-w-2xl xl:shrink-0">
           <p className="base-20 self-center text-light-100 xl:self-start">
             Beverages for connoisseurs
           </p>
-          <h1 className="hero-heading mt-5 text-center text-light-100 xl:text-start">
+          <h1 className="hero-heading">
             Raise Your Glass Begin a{" "}
             <span className="text-orange-light">Flavorful Cocktail</span>{" "}
             Journey
@@ -32,7 +32,7 @@ const Hero = () => {
             />
           </button>
         </div>
-        <figure className="flex h-fit w-full max-w-lg shrink-0 items-end xl:h-[44rem] xl:max-w-xl xl:pb-24 xl:pl-14">
+        <figure className="flex h-fit w-full max-w-lg shrink-0 items-end xl:mt-20 xl:max-w-xl xl:pl-14">
           <Image
             src="/drinks-graphic.png"
             alt="A refreshing orange cocktail in a glass"
