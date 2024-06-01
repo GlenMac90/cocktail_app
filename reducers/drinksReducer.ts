161118c5a003c85aa91379973391ae9c8c84f169
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-06-01 22:26:27 +0300

    remove unused reducer actions

--- reducers/drinksReducer.ts
+++ reducers/drinksReducer.ts
@@ -17,13 +17,8 @@ export const initialState: State = {
 };
 
 type Action =
-  | { type: "SET_DRINKS"; payload: DrinkData[] }
-  | { type: "ADD_DRINKS"; payload: DrinkData[] }
-  | { type: "SET_IS_MORE_POSTS"; payload: boolean }
-  | { type: "SET_SKIP"; payload: number }
   | { type: "SET_FETCHING"; payload: boolean }
   | { type: "SET_FILTER"; payload: DrinksFilters }
-  | { type: "RESET" }
   | {
       type: "SET_NEW_DRINKS";
       payload: { drinks: DrinkData[]; isMorePosts: boolean; skip: number };
@@ -32,26 +27,10 @@ type Action =
 
 export const drinksReducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_DRINKS":
-      return { ...state, drinks: action.payload };
-    case "ADD_DRINKS":
-      return { ...state, drinks: [...state.drinks, ...action.payload] };
-    case "SET_IS_MORE_POSTS":
-      return { ...state, isMorePosts: action.payload };
-    case "SET_SKIP":
-      return { ...state, skip: action.payload };
     case "SET_FETCHING":
       return { ...state, fetching: action.payload };
     case "SET_FILTER":
       return { ...state, filter: action.payload };
-    case "RESET":
-      return {
-        ...state,
-        drinks: [],
-        isMorePosts: true,
-        skip: 0,
-        filter: "all",
-      };
     case "SET_NEW_DRINKS":
       return {
         ...state,
